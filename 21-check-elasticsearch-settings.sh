5b33b59590fa58b413edd85791e9a3f28a71bc25
Theodor Li <theodor2311@gmail.com>
2020-08-06 20:56:04 +0800

    Adding checking scripts by directly check from Elasticsearch instance

---
+++ 21-check-elasticsearch-settings.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -euoE pipefail
+readonly ELASTICSEARCH_INSTANCE=$(oc get pods -lcomponent=elasticsearch -n openshift-logging -o jsonpath='{.items[*].metadata.name}')
+[[ -z "${ELASTICSEARCH_INSTANCE}" ]] && echo 'Elasticsearch instance not found' && exit 1
+echo 'Changing project to openshift-logging Project...'
+oc project openshift-logging
+oc exec "${ELASTICSEARCH_INSTANCE}" -c elasticsearch -- es_util --query=_cluster/settings?pretty=true
