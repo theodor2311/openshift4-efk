3454891c5d720e3cbf246cf7986bd2a786cf3757
Theodor Li <50130439+theodor2311@users.noreply.github.com>
2020-08-25 11:32:10 +0800

    Update 99-remove-efk.sh

--- 99-remove-efk.sh
+++ 99-remove-efk.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+oc delete clusterlogging instance -n openshift-logging
 oc delete sub -n openshift-logging cluster-logging
 oc delete sub -n openshift-operators elasticsearch-operator
 oc delete project openshift-logging
+oc delete project openshift-operators-redhat
+oc delete rolebindings prometheus-k8s -n openshift-operators-redhat
+oc delete role prometheus-k8s -n openshift-operators-redhat
