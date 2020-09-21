d943929848eb5233dd32fb8d131813db8492268d
Theodor Li <50130439+theodor2311@users.noreply.github.com>
2020-09-21 20:44:29 +0800

    Create es_util.sh

---
+++ es_util.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -euoE pipefail
+if [[ $# -eq 0 ]] ; then
+    echo 'Usage: ./es_query --query=<QUERY>'
+    exit 0
+fi
+
+readonly ELASTICSEARCH_INSTANCE=$(oc get pods -lcomponent=elasticsearch -n openshift-logging -o jsonpath='{.items[0].metadata.name}')
+[[ -z "${ELASTICSEARCH_INSTANCE}" ]] && echo 'Elasticsearch instance not found' && exit 1
+oc project openshift-logging >/dev/null
+oc exec "${ELASTICSEARCH_INSTANCE}" -c elasticsearch -- es_util $@
